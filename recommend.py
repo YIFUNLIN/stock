d17ff4bbde2f25e2d2107c2f1349689777091f6d
YIFUNLIN <drose01rrr@gmail.com>
2024-04-22 12:41:11 +0800

    n

--- recommend.py
+++ recommend.py
@@ -7,29 +7,29 @@ from strategy.grid import trade
 
 def recommend_stock(url, parameters):
     df = pd.read_csv(url, index_col='Datetime')
-    df.columns = map(str.lower, df.columns)
-    df['open'] = pd.to_numeric(df['open'], errors='coerce')
+    df.columns = map(str.lower, df.columns) # 將所有欄位名轉換為小寫，確保後續處理中列名的一致性
+    df['open'] = pd.to_numeric(df['open'], errors='coerce') # 將股票資料的欄位轉換為數值類型。如果轉換失敗，則用 NaN 填充
     df['high'] = pd.to_numeric(df['high'], errors='coerce')
     df['low'] = pd.to_numeric(df['low'], errors='coerce')
     df['close'] = pd.to_numeric(df['close'], errors='coerce')
     df['volume'] = pd.to_numeric(df['volume'], errors='coerce')
 
-    states_buy, states_sell, states_entry, states_exit, total_gains, invest = trade(df, **parameters)
+    states_buy, states_sell, states_entry, states_exit, total_gains, invest = trade(df, **parameters) # 執行交易策略
 
-    today = len(df)
-    today_close_price = df.close.iloc[-1]
+    today = len(df) # 總交易天數
+    today_close_price = df.close.iloc[-1] # 抓最後一筆的收盤價
 
     should_buy = abs(today - states_buy[-1]) < 27
-    should_sell = abs(today - states_sell[-1]) < 27
+    should_sell = abs(today - states_sell[-1]) < 27   # 若最後一次賣出到目前的天數小於27天，就認為應該進行賣出操作
 
     return should_buy, should_sell, today_close_price, total_gains
 
 
-def generate_report(urls, parameters, limit=10):
+def generate_report(urls, parameters, limit=10): # 此函數負責產生股票推薦的 HTML 報告
     results = []
     for url in urls:
         try:
-            should_buy, should_sell, today_close_price, total_gains = recommend_stock(url, parameters)
+            should_buy, should_sell, today_close_price, total_gains = recommend_stock(url, parameters) # 對於給定的 URL 列表，函數嘗試為每個 URL 執行 recommend_stock 並收集結果
             if should_sell or should_buy:
                 results.append({
                     "Stock": url.split('/')[-1].split('.')[0],
@@ -45,15 +45,27 @@ def generate_report(urls, parameters, limit=10):
     sorted_results = sorted(results, key=lambda x: x['Total_Gains'], reverse=True)[:limit]
 
     df = pd.DataFrame(sorted_results)
-    env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('stock_report_template.html')
-    html_output = template.render(stocks=df.to_dict(orient='records'))
 
-    with open('stock_report.html', 'w') as f:
+    # 使用 Jinja2 範本引擎和預先定義的 HTML 範本產生報告
+    env = Environment(loader=FileSystemLoader('templates')) 
+    # Environment 是 Jinja2 中的一個核心概念，它封裝了模板的配置和全域物件。這裡建立一個 Environment 實例是為了管理範本檔案。
+    # 使用 FileSystemLoader，指定 Jinja2 從哪個目錄下載入模板檔案。在這個範例中，模板檔案被放在名為 templates 的目錄中。
+    
+    template = env.get_template('stock_report_template.html') 
+    # 使用 get_template 方法從環境中載入一個名為 stock_report_template.html 的模板檔案。
+    # 這意味著你應該有一個這樣命名的 HTML 模板檔案存放在 templates 目錄中。
+
+    html_output = template.render(stocks=df.to_dict(orient='records')) # 模板渲染的過程，這裡將資料傳遞給模板進行渲染。
+    # 將 pandas DataFrame 轉換為字典列表，每個列表項目代表 DataFrame 中的一行，這使得資料可以在範本中按照預定的 HTML 結構進行展示
+
+
+    with open('stock_report.html', 'w') as f: 
         f.write(html_output)
+    # 將渲染後的 HTML 輸出儲存到一個名為 stock_report.html 的檔案中。使用 with open(...) 語句確保檔案正確開啟且最終會被關閉。 
+    # 'w' 參數表示以寫入模式開啟文件，如果文件已存在，則覆蓋原有內容。
 
-
-parameters = {
+# 交易參數設定和執行
+parameters = { 
     "rsi_period": 14,
     "low_rsi": 30,
     "high_rsi": 70,
--- update.py
+++ update.py
@@ -5,28 +5,38 @@ import pytz
 import vectorbt as vbt
 import twstock
 
-codes = twstock.codes
+'''
+這段程式碼是設計用於從 Yahoo Finance 自動更新台灣股市的股票和 ETF 資料，
+並將更新的數據保存為 CSV 文件。會將此工作被放在 GitHub Actions 中執行，以實現定期的資料更新
+'''
 
-def get_data_since_last_record(stock_num, base_path='./data/'):
+codes = twstock.codes #  獲取所有台灣股市的股票代碼及相關信息
+'''
+裡面長這樣
+700001': StockCodeInfo(type='上櫃認購(售)權證', code='700001', name='新普台新85購01', ISIN='TW18Z7000011', start='2018/10/01', market='上櫃', group='', CFI='RWSCCE'),
+ '700002': StockCodeInfo(type='上櫃認購(售)權證', code='700002', name='環球晶台新84購01', ISIN='TW18Z7000029', start='2018/10/01', market='上櫃', group='', CFI='RWSCCE')
+ '''
+
+def get_data_since_last_record(stock_num, base_path='./data/'): #  函式負責獲取指定股票號碼的最新數據
     csv_path = f'{base_path}{stock_num}.csv'
     tz_taipei = pytz.timezone('Asia/Taipei')
     today = datetime.now(tz_taipei).replace(hour=0, minute=0, second=0, microsecond=0)  # Reset to start of day
 
-    if os.path.exists(csv_path):
+    if os.path.exists(csv_path):  # 若csv存在，則讀取
         data = pd.read_csv(csv_path, header=0)
-        if not data.empty:
+        if not data.empty: # 若資料內不為空
             try:
-                last_record_date = pd.to_datetime(data['Datetime'].iloc[-1]).tz_convert('Asia/Taipei')
-                start_date = last_record_date + timedelta(minutes=5)
-            except Exception as e:
+                last_record_date = pd.to_datetime(data['Datetime'].iloc[-1]).tz_convert('Asia/Taipei') # 讀取最後一筆資料(以確認上次更新到哪)
+                start_date = last_record_date + timedelta(minutes=5) # 若成功讀取，將該時間(最後一筆資料日期)再加5分鐘，以獲取新數據
+            except Exception as e: # 若無法成功讀取，程式會從今天往回推59天開始抓取數據，一直抓取到當前的日期
                 print(f"Error parsing last record date: {e}")
                 start_date = today - timedelta(days=59)
         else:
-            start_date = today - timedelta(days=59)
+            start_date = today - timedelta(days=59) 
     else:
         start_date = today - timedelta(days=59)
 
-    end_date = today - timedelta(minutes=5)  # Reduce by 5 minutes to ensure data availability
+    end_date = today - timedelta(minutes=5)  # 每天會更新到13:55才停止 (資料的結束時間設定為當前時間減去五分鐘，以確保資料的可用性和完整性。)
 
     yf_data = vbt.YFData.download(
         f"{stock_num}.TW",
@@ -45,7 +55,7 @@ def get_data_since_last_record(stock_num, base_path='./data/'):
 
     return new_data
 
-for k, v in codes.items():
-    if v.market == '上市' and (v.type == '股票' or v.type == 'ETF'):
+for k, v in codes.items(): # 迭代更新股票數據
+    if v.market == '上市' and (v.type == '股票' or v.type == 'ETF'): # 只抓「上市」且類型為「股票」或「ETF」的股票
         new_data = get_data_since_last_record(k)
         print(f"Updated data for {k}")
